84ecf06cbbdb7c91a466fbe75d445d4fee49cf96
Isaque Pinheiro <isaquesp@gmail.com>
2019-03-12 11:34:02 -0300

    Add - Criado um instalador para o ORMBr suas funções são: a) Registar o ORMBr na tela principal do Delphi b) Add todos os paths librarys para que não precise fazer isso manualmente c) Instalar componentes usados no ORMBr, nessa versão irá instalar o MongoWire para acesso ao MongoDB, o que irá desmaecado por padrão.

---
+++ Projects/ORMBr Install/ORMBrInstall.dpr
@@ -0,0 +1,16 @@
+program ORMBrInstall;
+
+uses
+  Forms,
+  uPrincipal in 'uPrincipal.pas' {frmPrincipal},
+  uFrameLista in 'uFrameLista.pas' {framePacotes: TFrame};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Title := 'Instalação ORMBr - REST Server/Client Componentes';
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
---
+++ Projects/ORMBr Install/uFrameLista.pas
@@ -0,0 +1,158 @@
+﻿unit uFrameLista;
+
+interface
+
+uses
+  Generics.Collections,
+  Generics.Defaults,
+
+  Windows,
+  Messages,
+  SysUtils,
+  Variants,
+  Classes,
+  Graphics,
+  Controls,
+  Forms,
+  Dialogs,
+  Buttons,
+  ExtCtrls,
+  StdCtrls,
+  ComCtrls;
+
+type
+  TPacotes = TList<TCheckBox>;
+
+  TframePacotes = class(TFrame)
+    pnlBotoesMarcar: TPanel;
+    btnPacotesDesmarcarTodos: TSpeedButton;
+    btnPacotesMarcarTodos: TSpeedButton;
+    ScrollBox1: TScrollBox;
+    Label6: TLabel;
+    Bevel2: TBevel;
+    MongoDBCore_dpk: TCheckBox;
+    Label13: TLabel;
+    Label1: TLabel;
+    ORMBrLibrary_dpk: TCheckBox;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure btnPacotesMarcarTodosClick(Sender: TObject);
+    procedure btnPacotesDesmarcarTodosClick(Sender: TObject);
+    procedure VerificarCheckboxes(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
+  private
+    FPacotes: TPacotes;
+    FUtilizarBotoesMarcar: Boolean;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
+    property Pacotes: TPacotes read FPacotes write FPacotes;
+  end;
+
+implementation
+
+uses
+  StrUtils;
+
+{$R *.dfm}
+
+constructor TframePacotes.Create(AOwner: TComponent);
+var
+  I: Integer;
+begin
+  inherited;
+
+  // variavel para controle do verificar checkboxes
+  // utilizada para evitar estouro de pilha por conta da redundância
+  // e também para que pacotes dependentes não atrapalhem a rotina
+  FUtilizarBotoesMarcar := False;
+
+  // lista de pacotes (checkboxes) disponiveis
+  FPacotes := TPacotes.Create;
+
+  // popular a lista de pacotes com os pacotes disponíveis
+  // colocar todos os checkboxes disponíveis na lista
+  FPacotes.Clear;
+  for I := 0 to Self.ComponentCount - 1 do
+  begin
+    if Self.Components[I] is TCheckBox then
+      if TCheckBox(Self.Components[I]).Tag = 0 then
+         FPacotes.Add(TCheckBox(Self.Components[I]));
+  end;
+  FPacotes.Sort(TComparer<TCheckBox>.Construct(
+      function(const Dpk1, Dpk2: TCheckBox): Integer
+      begin
+         Result := CompareStr( FormatFloat('0000', Dpk1.TabOrder), FormatFloat('0000', Dpk2.TabOrder) );
+      end));
+end;
+
+destructor TframePacotes.Destroy;
+begin
+  FreeAndNil(FPacotes);
+
+  inherited;
+end;
+
+procedure TframePacotes.Label1Click(Sender: TObject);
+begin
+
+end;
+
+// botão para marcar todos os checkboxes
+procedure TframePacotes.btnPacotesMarcarTodosClick(Sender: TObject);
+var
+  I: Integer;
+begin
+  FUtilizarBotoesMarcar := True;
+  try
+    for I := 0 to Self.ComponentCount -1 do
+    begin
+      if Self.Components[I] is TCheckBox then
+      begin
+        if TCheckBox(Self.Components[I]).Enabled then
+          TCheckBox(Self.Components[I]).Checked := True;
+      end;
+    end;
+  finally
+    FUtilizarBotoesMarcar := False;
+    VerificarCheckboxes(Sender);
+  end;
+end;
+
+// botão para desmarcar todos os checkboxes
+procedure TframePacotes.btnPacotesDesmarcarTodosClick(Sender: TObject);
+var
+  I: Integer;
+begin
+  FUtilizarBotoesMarcar := True;
+  try
+    for I := 0 to Self.ComponentCount -1 do
+    begin
+      if Self.Components[I] is TCheckBox then
+      begin
+        if TCheckBox(Self.Components[I]).Enabled then
+          TCheckBox(Self.Components[I]).Checked := False;
+      end;
+    end;
+  finally
+    FUtilizarBotoesMarcar := False;
+    VerificarCheckboxes(Sender);
+  end;
+end;
+
+// rotina de verificação de dependência e marcação dos pacotes base
+procedure TframePacotes.VerificarCheckboxes(Sender: TObject);
+begin
+  MongoDBCore_dpk.Checked := True;
+  if not FUtilizarBotoesMarcar then
+  begin
+    FUtilizarBotoesMarcar := True;
+    try
+    finally
+      FUtilizarBotoesMarcar := False;
+    end;
+  end;
+end;
+
+end.
